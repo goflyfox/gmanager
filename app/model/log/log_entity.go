f326938a06782c03b553f21890889ca42a3ba7a4
zhangbiao <zhangbiao@channelsoft.com>
2019-12-31 16:42:48 +0800

    update log entity

--- app/model/log/log_entity.go
+++ app/model/log/log_entity.go
@@ -11,18 +11,21 @@ import (
 
 // Entity is the golang structure for table sys_log.
 type Entity struct {
-	Id         int    `orm:"id,primary"  json:"id"`          // 主键
-	LogType    int    `orm:"log_type"    json:"log_type"`    // 类型
-	OperObject string `orm:"oper_object" json:"oper_object"` // 操作对象
-	OperTable  string `orm:"oper_table"  json:"oper_table"`  // 操作表
-	OperId     int    `orm:"oper_id"     json:"oper_id"`     // 操作主键
-	OperType   string `orm:"oper_type"   json:"oper_type"`   // 操作类型
-	OperRemark string `orm:"oper_remark" json:"oper_remark"` // 操作备注
-	Enable     int    `orm:"enable"      json:"enable"`      // 是否启用//radio/1,启用,2,禁用
-	UpdateTime string `orm:"update_time" json:"update_time"` // 更新时间
-	UpdateId   int    `orm:"update_id"   json:"update_id"`   // 更新人
-	CreateTime string `orm:"create_time" json:"create_time"` // 创建时间
-	CreateId   int    `orm:"create_id"   json:"create_id"`   // 创建者
+	Id         int    `orm:"id,primary"  json:"id"`         // 主键
+	LogType    int    `orm:"log_type"    json:"logType"`    // 类型
+	OperObject string `orm:"oper_object" json:"operObject"` // 操作对象
+	OperTable  string `orm:"oper_table"  json:"operTable"`  // 操作表
+	OperId     int    `orm:"oper_id"     json:"operId"`     // 操作主键
+	OperType   string `orm:"oper_type"   json:"operType"`   // 操作类型
+	OperRemark string `orm:"oper_remark" json:"operRemark"` // 操作备注
+	Enable     int    `orm:"enable"      json:"enable"`     // 是否启用//radio/1,启用,2,禁用
+	UpdateTime string `orm:"update_time" json:"updateTime"` // 更新时间
+	UpdateId   int    `orm:"update_id"   json:"updateId"`   // 更新人
+	CreateTime string `orm:"create_time" json:"createTime"` // 创建时间
+	CreateId   int    `orm:"create_id"   json:"createId"`   // 创建者
+
+	UpdateName string `json:"updateName,omitempty" gconv:"updateName,omitempty"`
+	CreateName string `json:"createName,omitempty" gconv:"createName,omitempty"`
 }
 
 // OmitEmpty sets OPTION_OMITEMPTY option for the model, which automatically filers
